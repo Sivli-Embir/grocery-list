1a83c02060fbb9b7c64ca910da98d0366159a672
Emma <emma@Conners-MacBook-Pro.local>
2015-10-17 17:48:58 -0400

    added ionic

--- grocery-list.js
+++ grocery-list.js
@@ -1,23 +1,5 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+Router.route('/', function () {
+  this.render('ListView', {
+    // data: function () { return Items.findOne({_id: this.params._id}); }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+});
